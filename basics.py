9a7e5f5c53f776a7009bfae05b0160c275fff0cc
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-03-30 22:01:02 +0500

    interpolation and mutable and immutable

--- basics.py
+++ basics.py
@@ -2,3 +2,21 @@ print("Hello Sadam")
 def sadam(n):
     print(n)
 sadam('sadam Mueer')
+
+
+# mutable and immutable ?
+
+username = "sadam"
+username = "Hamza"
+print(username)
+
+# In Python, you can use f-strings (formatted string literals) to achieve template-like string interpolation.
+a = 5
+b = a
+print(f"The value of A is : {a}" )
+print(f"The value of B is : {b}" )
+
+a = 10
+print(f"The value of A is : {a}" )
+print(f"The value of B is : {b}" )
+
--- basics2.py
+++ basics2.py
@@ -1,2 +1,11 @@
 from basics import sadam
+import os
+# imported fucntion
 sadam('sadam')
+
+ # for loop
+
+for name in "sadam":
+    print(name)
+# os 
+print(os.getcwd())
